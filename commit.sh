b647f6b8adcb0bcb3b849b21a39ff9733d0f1dae
Fernando Almeida <fernando.c.almeida@gmail.com>
2025-07-10 00:54:50 -0300

    Updates

--- commit.sh
+++ commit.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+SCRIPT=$(readlink -f "$0")
+DIR=$(dirname "$SCRIPT")
+source ${DIR}/subs/constants.sh
+source ${DIR}/subs/functions.sh
+
 SOURCE_DIR="/home/pi/scripts/"
 DEST_DIR="/home/pi/projects/shell/raspberry_pi_scripts"
 EXCLUDE_FILE="$SOURCE_DIR/exclude-list.txt"
 
+info "Synching scripts project"
+
 /usr/bin/rsync -av --exclude-from $EXCLUDE_FILE $SOURCE_DIR $DEST_DIR
+
+info "Commiting changes"
+
+cd $DEST_DIR
+git status
+
+info "Completed"
 
 # EOF
