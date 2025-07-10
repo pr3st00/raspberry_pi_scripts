ad9e2bc8dd7d8364383d97c8ccc6cd61ef4cb545
Fernando Almeida <fernando.c.almeida@gmail.com>
2025-07-10 00:58:20 -0300

    Updated scripts at Thu 10 Jul 00:58:17 -03 2025

--- commit.sh
+++ commit.sh
@@ -9,14 +9,25 @@ SOURCE_DIR="/home/pi/scripts/"
 DEST_DIR="/home/pi/projects/shell/raspberry_pi_scripts"
 EXCLUDE_FILE="$SOURCE_DIR/exclude-list.txt"
 
+COMMIT_MESSAGE="Updated scripts at $(date)" 
+
 info "Synching scripts project"
 
 /usr/bin/rsync -av --exclude-from $EXCLUDE_FILE $SOURCE_DIR $DEST_DIR
-
-info "Commiting changes"
-
 cd $DEST_DIR
+
+info "Checking for changes"
 git status
+
+info "Commit changes?"
+echo "RESP: [y|n]"
+read RESP
+
+if [[ $RESP -eq "y" ]]; then
+	git add .
+	git commit -m"$COMMIT_MESSAGE"
+	git push
+fi
 
 info "Completed"
 
